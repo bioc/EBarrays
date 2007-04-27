1c9279f591e0b03c001b93e0899cc3c2fbeb6540
Deepayan Sarkar <dsarkar@fhcrc.org>
2007-04-27 20:03:58 +0000

    fixed bug related to INSTALL --with-package-versions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_0/madman/Rpacks/EBarrays@24347 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -19,8 +19,7 @@
 ### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ### MA 02111-1307, USA
 
-
 .First.lib <- function(lib, pkg) {
-    library.dynam(pkg, pkg, lib )
+    library.dynam("EBarrays", pkg, lib)
 }
 
